1766d834cb1823bd7f8c5e5258df2674439230f0
carolpinheiro <ommasshi@gmail.com>
2021-03-04 21:58:49 -0300

    configurações iniciais de lint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-type Props = {
-  title: 'string'
-}
-
-export default function Home({title}: Props) {
+export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
